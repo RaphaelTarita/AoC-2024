d8a3bdee97a2dd365ad486d83f5725deca911000
Raphael Tarita <raphael.tarita@gmail.com>
2024-12-04 07:17:40 +0100

    [Day 2] solution

--- src/main/kotlin/com/rtarita/days/Day2.kt
+++ src/main/kotlin/com/rtarita/days/Day2.kt
@@ -7,11 +7,27 @@ import kotlinx.datetime.LocalDate
 object Day2 : AoCDay {
     override val day: LocalDate = day(2)
 
+    private val DECREASE_RANGE = -3..-1
+    private val INCREASE_RANGE = 1..3
+
+    private fun parseReports(input: String) = input.lines()
+        .map { it.split(" ") }
+        .map { report -> report.map { it.toInt() } }
+
+    private fun checkDeltas(levels: List<Int>): Boolean {
+        val deltas = levels.zipWithNext { a, b -> b - a }
+        return deltas.all { it in DECREASE_RANGE } || deltas.all { it in INCREASE_RANGE }
+    }
+
     override fun executePart1(input: String): Any {
-        return "(not implemented)"
+        return parseReports(input).count { checkDeltas(it) }
     }
 
     override fun executePart2(input: String): Any {
-        return "(not implemented)"
+        return parseReports(input).count { levels ->
+            checkDeltas(levels) || levels.indices.any { lvlIdx ->
+                checkDeltas(levels.filterIndexed { idx, _ -> idx != lvlIdx })
+            }
+        }
     }
 }
--- src/main/kotlin/com/rtarita/main.kt
+++ src/main/kotlin/com/rtarita/main.kt
@@ -3,6 +3,6 @@ package com.rtarita
 import com.rtarita.structure.AoC
 
 fun main() {
-    println(AoC.executeSimple(1).formatResult())
+    println(AoC.executeSimple(2).formatResult())
     AoC.printAllTimed(100, 10)
 }
